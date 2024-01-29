a2bdf4e6d25879a47d1ce68ae6f91d082b5f678c
Rustem Khalilov <rvkhalilov@gmail.com>
2024-01-30 02:52:54 +0500

    fix bag 4-е задание

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,8 +4,16 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 0, "test") == 1
+    assert arrs.get([1, 2, 3], 2, "test") == 3
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], ) == []
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([], None) == []
+    assert arrs.my_slice([], 1 , 3) == []
+    assert arrs.my_slice([1, 2, 3, 4], 0, 10) == [1, 2, 3, 4]
+    assert arrs.my_slice([1, 2, 3, 4]) == [1, 2, 3, 4]
